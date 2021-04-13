c21c80b933e3f9ab5927e10439a671dec5fe52d0
kotala-sudo <pkotala@yahoo.com>
2021-04-13 17:51:43 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { StudentListComponent } from './student-list/student-list.component';
+import { AppComponent } from "./app.component";
+import { StudentListComponent } from "./student-list/student-list.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    StudentListComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, StudentListComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
