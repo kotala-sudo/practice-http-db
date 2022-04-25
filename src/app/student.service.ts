1c6071a4da71300d90d00c859305a47b687bb26e
kotala-sudo <pkotala@yahoo.com>
2022-04-25 14:36:07 -0500

    1 file modified

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,31 +1,16 @@
-import { Injectable } from "@angular/core";
-import { Student } from "./student";
+import { Injectable } from '@angular/core';
+import { Student } from './student';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class StudentService {
   STUDENTS: Student[] = [
-    {
-      id: 1,
-      name: "John",
-      age: 20
-    },
-    {
-      id: 2,
-      name: "Emily",
-      age: 21
-    },
-    {
-      id: 3,
-      name: "Jeremy",
-      age: 19
-    },
-    {
-      id: 4,
-      name: "Mary",
-      age: 18
-    }
+    { id: 1, name: 'John', age: 20 },
+    { id: 2, name: 'Emily', age: 21 },
+    { id: 3, name: 'Geeta', age: 19 },
+    { id: 4, name: 'Ahmed', age: 18 },
+    { id: 5, name: 'Juan', age: 22 },
   ];
 
   constructor() {}
